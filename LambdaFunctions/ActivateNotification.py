6128f5e8d65b34a38b07942311ae8de63ad6dc21
Wayne122 <ww040702tw01@gmail.com>
2021-04-30 08:47:48 -0700

    User can now be created without token

--- LambdaFunctions/ActivateNotification.py
+++ LambdaFunctions/ActivateNotification.py
@@ -59,72 +59,86 @@ def lambda_handler(event, context):
                         Key=id
                     )
 
-                subInfo = id
+                # Creation process
+                # If token exists
+                if 'deviceTokenId' in userInfo:
+                    subInfo = id
 
-                response = client.create_platform_endpoint(
-                    PlatformApplicationArn='arn:aws:sns:us-east-1:756906170378:app/APNS_SANDBOX/iOS_Emergency_Indoor_Nav',
-                    Token=userInfo['deviceTokenId'],
-                    CustomUserData=userInfo['id']
-                )
-
-                if "EndpointArn" in response:
-                    subInfo['EndpointArn'] = response['EndpointArn']
-
-                    # Get all topics
-                    topics = client.list_topics()
-                    tlist = []
-                    for topic in topics['Topics']:
-                        if topic['TopicArn'].split(':')[-1].split('_')[0] == "SmartNavigationPushNotification":
-                            tlist.append(topic['TopicArn'])
-                    
-                    # Find an usable topic
-                    tArn = ""
-                    for t in tlist:
-                        r = client.get_topic_attributes(TopicArn=t)
-                        if int(r['Attributes']['SubscriptionsConfirmed']) < 10000000:
-                            tArn = t
-                            break
-
-                    # Create topic if no topic is found
-                    if not tArn:
-                        newTopic = client.create_topic(
-                            Name='SmartNavigationPushNotification_' + str(len(tlist))
-                        )
-                        tArn = newTopic['TopicArn']
-                    
-                    response = client.subscribe(
-                        TopicArn=tArn,
-                        Protocol='application',
-                        Endpoint=subInfo['EndpointArn'],
-                        ReturnSubscriptionArn=True
+                    # Create endpoint
+                    response = client.create_platform_endpoint(
+                        PlatformApplicationArn='arn:aws:sns:us-east-1:756906170378:app/APNS_SANDBOX/iOS_Emergency_Indoor_Nav',
+                        Token=userInfo['deviceTokenId'],
+                        CustomUserData=userInfo['id']
                     )
 
-                    if "SubscriptionArn" in response:
-                        subInfo['SubscriptionArn'] = response['SubscriptionArn']
+                    # If endpoint creation succeeded
+                    if "EndpointArn" in response:
+                        subInfo['EndpointArn'] = response['EndpointArn']
 
-                        response = subtable.put_item(
-                            Item=subInfo
+                        # Get all topics
+                        topics = client.list_topics()
+                        tlist = []
+                        for topic in topics['Topics']:
+                            if topic['TopicArn'].split(':')[-1].split('_')[0] == "SmartNavigationPushNotification":
+                                tlist.append(topic['TopicArn'])
+                        
+                        # Find an usable topic
+                        tArn = ""
+                        for t in tlist:
+                            r = client.get_topic_attributes(TopicArn=t)
+                            if int(r['Attributes']['SubscriptionsConfirmed']) < 10000000:
+                                tArn = t
+                                break
+
+                        # Create topic if no topic is found
+                        if not tArn:
+                            newTopic = client.create_topic(
+                                Name='SmartNavigationPushNotification_' + str(len(tlist))
+                            )
+                            tArn = newTopic['TopicArn']
+
+                        # Subscribe to a topic
+                        response = client.subscribe(
+                            TopicArn=tArn,
+                            Protocol='application',
+                            Endpoint=subInfo['EndpointArn'],
+                            ReturnSubscriptionArn=True
                         )
 
-                        return {
-                            "statusCode": 200,
-                            "body": json.dumps({
-                                #"detail": token,
-                                "response": "Activated!"
-                            }),
-                        }
+                        # If topic subscription succeeded
+                        if "SubscriptionArn" in response:
+                            subInfo['SubscriptionArn'] = response['SubscriptionArn']
+
+                            response = subtable.put_item(
+                                Item=subInfo
+                            )
+
+                            return {
+                                "statusCode": 200,
+                                "body": json.dumps({
+                                    #"detail": token,
+                                    "response": "Activated!"
+                                }),
+                            }
+                        else:
+                            return {
+                                "statusCode": 400,
+                                "body": json.dumps({
+                                    "response": "Subscription unsuccessful.",
+                                }),
+                            }
                     else:
                         return {
                             "statusCode": 400,
                             "body": json.dumps({
-                                "response": "Subscription unsuccessful.",
+                                "response": "Endpoint creation unsuccessful.",
                             }),
                         }
                 else:
                     return {
                         "statusCode": 400,
                         "body": json.dumps({
-                            "response": "Endpoint creation unsuccessful.",
+                            "response": "No token found.",
                         }),
                     }
             else:
@@ -160,58 +174,65 @@ def lambda_handler(event, context):
                             Key={'id': userInfo['id']['S']}
                         )
 
-                    subInfo = {'id': userInfo['id']['S']}
+                    # Creation process
+                    # If token exists
+                    if 'deviceTokenId' in userInfo:
+                        subInfo = {'id': userInfo['id']['S']}
 
-                    response = client.create_platform_endpoint(
-                        PlatformApplicationArn='arn:aws:sns:us-east-1:756906170378:app/APNS_SANDBOX/iOS_Emergency_Indoor_Nav',
-                        Token=userInfo['deviceTokenId']['S'],
-                        CustomUserData=userInfo['id']['S']
-                    )
-
-                    if "EndpointArn" in response:
-                        subInfo['EndpointArn'] = response['EndpointArn']
-
-                        # Get all topics
-                        topics = client.list_topics()
-                        tlist = []
-                        for topic in topics['Topics']:
-                            if topic['TopicArn'].split(':')[-1].split('_')[0] == "SmartNavigationPushNotification":
-                                tlist.append(topic['TopicArn'])
-                        
-                        # Find an usable topic
-                        tArn = ""
-                        for t in tlist:
-                            r = client.get_topic_attributes(TopicArn=t)
-                            if int(r['Attributes']['SubscriptionsConfirmed']) < 10000000:
-                                tArn = t
-                                break
-
-                        # Create topic if no topic is found
-                        if not tArn:
-                            newTopic = client.create_topic(
-                                Name='SmartNavigationPushNotification_' + str(len(tlist)+1)
-                            )
-                            tArn = newTopic['TopicArn']
-
-                        response = client.subscribe(
-                            TopicArn=tArn,
-                            Protocol='application',
-                            Endpoint=subInfo['EndpointArn'],
-                            ReturnSubscriptionArn=True
+                        # Create endpoint
+                        response = client.create_platform_endpoint(
+                            PlatformApplicationArn='arn:aws:sns:us-east-1:756906170378:app/APNS_SANDBOX/iOS_Emergency_Indoor_Nav',
+                            Token=userInfo['deviceTokenId']['S'],
+                            CustomUserData=userInfo['id']['S']
                         )
 
-                        if "SubscriptionArn" in response:
-                            subInfo['SubscriptionArn'] = response['SubscriptionArn']
+                        # If endpoint creation succeeded
+                        if "EndpointArn" in response:
+                            subInfo['EndpointArn'] = response['EndpointArn']
 
-                            subtable.put_item(
-                                Item=subInfo
+                            # Get all topics
+                            topics = client.list_topics()
+                            tlist = []
+                            for topic in topics['Topics']:
+                                if topic['TopicArn'].split(':')[-1].split('_')[0] == "SmartNavigationPushNotification":
+                                    tlist.append(topic['TopicArn'])
+                            
+                            # Find an usable topic
+                            tArn = ""
+                            for t in tlist:
+                                r = client.get_topic_attributes(TopicArn=t)
+                                if int(r['Attributes']['SubscriptionsConfirmed']) < 10000000:
+                                    tArn = t
+                                    break
+
+                            # Create topic if no topic is found
+                            if not tArn:
+                                newTopic = client.create_topic(
+                                    Name='SmartNavigationPushNotification_' + str(len(tlist)+1)
+                                )
+                                tArn = newTopic['TopicArn']
+
+                            # Subscribe to a topic
+                            response = client.subscribe(
+                                TopicArn=tArn,
+                                Protocol='application',
+                                Endpoint=subInfo['EndpointArn'],
+                                ReturnSubscriptionArn=True
                             )
+
+                            # If topic subscription succeeded
+                            if "SubscriptionArn" in response:
+                                subInfo['SubscriptionArn'] = response['SubscriptionArn']
+
+                                subtable.put_item(
+                                    Item=subInfo
+                                )
                 elif r['eventName'] == "MODIFY":
                     newUserInfo = r['dynamodb']['NewImage']
                     oldUserInfo = r['dynamodb']['OldImage']
 
                     # Check if token is updated
-                    if newUserInfo['deviceTokenId']['S'] != oldUserInfo['deviceTokenId']['S']:
+                    if 'deviceTokenId' not in oldUserInfo and 'deviceTokenId' in newUserInfo or newUserInfo['deviceTokenId']['S'] != oldUserInfo['deviceTokenId']['S']:
                         # Delete old subscription
                         response = subtable.get_item(
                             Key={'id': newUserInfo['id']['S']}
